fd76192e1d641d4f024eb1e60f9201222dc0865c
Baurzhan <zetbaur@gmail.com>
2023-03-30 11:23:36 +0600

    react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,24 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import MainLayout from './layouts/MainLayout';
+import Home from './screens/Home';
+
+import './App.scss';
+
 function App() {
-  return <div className='App'>React</div>;
+  return (
+    <BrowserRouter>
+      <div className='App'>
+        <Routes>
+          <Route path='/' element={<MainLayout />}>
+            <Route index element={<Home />} />
+
+            {/* <Route path='courses/:courseSlug' element={<SingleCourse />} /> */}
+            {/* <Route path='*' element={<NotFound />} /> */}
+          </Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
