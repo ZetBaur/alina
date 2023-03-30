6d361f6dd16dd78230125959f1930e58c526a507
Baurzhan <zetbaur@gmail.com>
2023-03-30 22:18:39 +0600

    chart wrapper

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -1,3 +1,5 @@
+import styles from './Chart.module.scss';
+
 import {
   AreaChart,
   Area,
@@ -7,45 +9,39 @@ import {
   Tooltip,
   ResponsiveContainer,
 } from 'recharts';
+
 import { data } from '../data/chart.data';
 
 function Chart() {
   return (
-    <ResponsiveContainer width='100%' height='100%'>
-      <AreaChart
-        width={500}
-        height={400}
-        data={data}
-        margin={{
-          top: 10,
-          right: 30,
-          left: 0,
-          bottom: 0,
-        }}
-      >
-        <CartesianGrid strokeDasharray='3 3' />
-        <XAxis dataKey='name' />
-        <YAxis />
-        <Tooltip />
+    <div className={styles.chart}>
+      <div>График курса валют</div>
+      <ResponsiveContainer width='100%' height='100%'>
+        <AreaChart width={500} height={400} data={data}>
+          <CartesianGrid strokeDasharray='3 3' />
+          <XAxis dataKey='name' />
+          <YAxis />
+          <Tooltip />
 
-        <Area
-          type='monotone'
-          dataKey='pv'
-          stackId='1'
-          stroke='#54C3A3'
-          strokeWidth={2}
-          fill='#A4B6D5'
-        />
-        <Area
-          type='monotone'
-          dataKey='uv'
-          stackId='1'
-          stroke='#E58C91'
-          strokeWidth={2}
-          fill='#CEDDEB'
-        />
-      </AreaChart>
-    </ResponsiveContainer>
+          <Area
+            type='monotone'
+            dataKey='pv'
+            stackId='1'
+            stroke='#54C3A3'
+            strokeWidth={2}
+            fill='#A4B6D5'
+          />
+          <Area
+            type='monotone'
+            dataKey='uv'
+            stackId='1'
+            stroke='#E58C91'
+            strokeWidth={2}
+            fill='#CEDDEB'
+          />
+        </AreaChart>
+      </ResponsiveContainer>
+    </div>
   );
 }
 
