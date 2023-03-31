3e179c8f11c77a1244820a0f36ecbffecbbe1866
Baurzhan <zetbaur@gmail.com>
2023-03-31 11:25:39 +0600

    x axis display logic

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -1,3 +1,4 @@
+import { FunctionComponent, PureComponent } from 'react';
 import {
   AreaChart,
   Area,
@@ -10,13 +11,36 @@ import {
 
 import { data } from '../data/chart.data';
 
+const CustomizedAxisTick: FunctionComponent<any> = (props: any) => {
+  const { x, y, payload } = props;
+
+  return (
+    <g transform={`translate(${x},${y})`}>
+      <text dy={16} textAnchor='end' fill='#666'>
+        {payload.value[0] === '0'
+          ? payload.value.slice(1, 2)
+          : payload.value.slice(0, 2)}
+      </text>
+    </g>
+  );
+};
+
 function Chart() {
   return (
     <ResponsiveContainer width='100%' height='100%'>
       <AreaChart width={500} height={400} data={data}>
         <CartesianGrid strokeDasharray='10' vertical={false} />
-        <XAxis dataKey='name' axisLine={false} tickLine={false} />
+
+        <XAxis
+          dataKey='name'
+          axisLine={false}
+          tickLine={false}
+          tick={<CustomizedAxisTick />}
+          // interval={0}
+        />
+
         <YAxis axisLine={false} tickLine={false} />
+
         <Tooltip
           contentStyle={{
             background: '#30528B',
